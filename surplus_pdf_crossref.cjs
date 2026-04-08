98565203fca89cdcb35a3e4aa71d490f6bb98a7e
justinlbrj23 <90680205+justinlbrj23@users.noreply.github.com>
2026-04-08 11:54:40 +0800

    Update surplus_pdf_crossref.cjs

--- surplus_pdf_crossref.cjs
+++ surplus_pdf_crossref.cjs
@@ -6,7 +6,7 @@ const pdfParse = require("pdf-parse");
 // CONFIG
 // ==============================
 const PDF_URL =
-  "https://img1.wsimg.com/blobby/go/dcf61f4f-af9a-47b3-9cc6-0b2cf55ca500/downloads/56915ba8-9cb9-435f-b6ae-795e06090f5f/Surplus%20File%20Update%20List.pdf?ver=1774497696118";
+  "https://img1.wsimg.com/blobby/go/dcf61f4f-af9a-47b3-9cc6-0b2cf55ca500/downloads/3b7ed5a5-78e9-48b3-8706-b5167b71a8b7/Surplus%20File%20Update%20List.pdf?ver=1775098571392";
 
 const SPREADSHEET_ID = "1CsLXhlNp9pP9dAVBpGFvEnw1PpuUvLfypFg56RrgjxA";
 const SHEET_NAME = "raw_main";
